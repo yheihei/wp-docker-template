2150b54d81e9c3dd8e04ed0702880a26b2a11854
Yohei Kokubo <yhei.register@gmail.com>
2020-09-29 23:06:47 +0900

    テストコードが既に入っているテーマを使うと初期化ができない #6

--- init_wp_test.sh
+++ init_wp_test.sh
@@ -5,7 +5,7 @@ cd /var/www/html/wp-content/themes/
 for THEME_NAME in `ls -d */`
 do
   echo $THEME_NAME
-  if [ ! -e ./$THEME_NAME/tests ]; then
+  if [ ! -e ./$THEME_NAME/bin/install-wp-tests.sh ]; then
     echo "$THEME_NAME のテスト初期化"
     wp scaffold theme-tests --dir=/var/www/html/wp-content/themes/$THEME_NAME --allow-root
     cp /var/www/html/init_wp_theme_test.sh /var/www/html/wp-content/themes/$THEME_NAME/init_wp_theme_test.sh
